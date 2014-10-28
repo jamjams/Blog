1ad440a15afc3dd09b0ffca958176a0f23d97606
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2014-10-28 11:17:08 -0700

    push of blog.php

--- blog.php
+++ blog.php
@@ -8,8 +8,7 @@
   	# operator
 	print "<h2>Php blog</h2><br/>";
 	
- //allows us to connect to the controller folder
+ //allows us to connect to the controller folder. 
 	require_once(__DIR__ . "/controller/create-db.php");
-
 
 ?>
