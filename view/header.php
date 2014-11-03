e71d00877203685beca764321b633a01766145b7
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2014-11-03 12:12:27 -0800

    finished video 16

---
+++ view/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<!--
+putting the ending tags of the head and body in the footer.php file allows us to edit both files at the same time. They are connected.
+-->
+</head>
+<body>
+
