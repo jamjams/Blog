9dd4ce5a216be4d4cb2853af552b61ee624f43d2
Jah'melah Maat-Hotep <jahmelah.maathotep@gmail.com>
2014-11-05 11:24:28 -0800

    video 19

---
+++ view/form.php
@@ -0,0 +1,18 @@
+<h1>Create Blog Post</h1>
+
+<!-- WE want the form to take in certain information and put that info in a table -->
+<form>
+<!-- div box for title -->
+	<div>
+	<!-- text displayed in front of text box -->
+		<label for="title">Title: </label>
+		<!-- input box allows us to put text in the box -->
+		<input type="txt" name="title" />
+	</div>
+<!-- div box for posts -->
+	<div>
+		<label for="post">Post: </label>
+		<!-- we can put as much text aws we want which differs from the input box -->
+		<textarea name="post"></textarea>
+	</div>
+</form>
